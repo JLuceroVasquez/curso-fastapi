cd33490c0debd0775fac4f4595a3f62b834fe3d8
JLuceroVasquez <jim.octavio2016@gmail.com>
2025-09-09 17:03:25 -0500

    Actualización del 9SEP25 a las 17:03 horas

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from typing import Union
+#Ejecutar "uvicorn --port 8000 --reload --log-level info main:app" en Powershell para correr Uvicorn, el servidor web que se comunicará con la API para pasar y responder solicitudes de clientes http o https.
+#"from typing import Union" Se remplazó por el uso del operador "|" admitido en versiones superiores a Python 10.
 from fastapi import FastAPI
+from pydantic import BaseModel #Se importa la clase BaseModel para realizar una solicitud de tipo PUT
 
 #Creación de una aplicación mediante una instancia de FastAPI()
 app = FastAPI()
 
-#Método GET para retornar la respuesta de la función ... al cliente HTPSS
+#Creación de la clase Item a partir de BaseModel que servirá como esquema para actualizar items mediante solicitudes PUT
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool|None=None
+
+
+#Método GET para retornar la respuesta de la función ... al cliente HTPSS, sin modificar los datos del recurso.
 
 #Función holamundo
 @app.get('/')
 def holamundo():
-    return {'!Hola23':'Mundo'}
+    return {'!Hola123':'Mundo'}
+
+#Función leer_items, que emplea parámetros de ruta y consulta
+#Ejemplo para el navegador: localhost:8000/items/1000?q=Mouse
+@app.get('/items/{item_id}')
+def leer_items(item_id:int, q:str|None=None):
+    return {"item_id":item_id, "q":q}
+
+#Función sumar_operandos, que emplea parámetros de consulta
+#Ejemplo para el navegador: localhost:8000/suma?operando_1=5&operando_2=7
+@app.get('/suma')
+def sumar_operandos(operando_1:int, operando_2:int):
+    return {'suma':operando_1+operando_2}
+
+#Método PUT para actualizar los datos del recurso mediante la función ... que respeta esquemas de clase.
+
+#Función actualizar_item
+#Se probó su funcionamiento en la página de la documentación generada por FastAPI
+@app.put('/item/{item_id}')
+def actualizar_item(item_id:int, item: Item):
+    return {'item_id':item_id,
+            'name':item.name,
+            'is_offer':item.is_offer}
